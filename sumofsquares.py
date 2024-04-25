65badd6143141a1b6c8065107e94a36e419cb680
ChibusonmaUdensi <165843794+ChibusonmaUdensi@users.noreply.github.com>
2024-04-25 08:58:22 +0100

    sumofsquares.py

--- sumofsquares.py
+++ sumofsquares.py
@@ -1,8 +1,6 @@
-def sum_of_squaress(numbers):
-    sum_of_squares = 0
-    for num in range(end_number + 1):
-        sum_of_squares += num ** 2
-    return sum_of_squares
+def sum_of_squaress(list):
+	squares =sum([i ** 2 for i in list])
+	return squares
 
-print(sum_of_squaress)
+print(sum_of_squaress([2, 3, 4, 5, 7]))
 
