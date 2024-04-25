db82ac70792e25b2b98962d84d2a75e496bf8790
ChibusonmaUdensi <165843794+ChibusonmaUdensi@users.noreply.github.com>
2024-04-25 08:29:21 +0100

    square

---
+++ squaree.py
@@ -0,0 +1,2 @@
+def square(list):
+    return [i ** 2 for i in list]
---
+++ sumofsquares.py
@@ -0,0 +1,8 @@
+def sum_of_squaress(numbers):
+    sum_of_squares = 0
+    for num in range(end_number + 1):
+        sum_of_squares += num ** 2
+    return sum_of_squares
+
+print(sum_of_squaress)
+
